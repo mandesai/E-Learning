0f4cfc00aedbed52216484468ff6f47cf34b5a3c
Man Desai <mandesai555@gmail.com>
2024-05-24 17:26:28 +0530

    add quiz in admin

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import PayoutStatement
+from .models import PayoutStatement, AssignmentResult, Result
 
+admin.site.register(AssignmentResult)
+admin.site.register(Result)
 admin.site.register(PayoutStatement)
